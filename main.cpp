808f0baef0e3da12cab74ac82c83f70ae69dd096
lk <2461810678@qq.com>
2024-12-04 13:30:01 +0800

    创建main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[],int)
+{
+	
+}
+
+int main()
+{
+	int arr[]={1,2,3,4};
+	int size = sizeof(arr)/sizeof((arr[0]);
+	sort(arr,size);
+	return 0;
+}
